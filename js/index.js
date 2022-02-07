656967ccc86f29b8b2cb4f345e349d005ac54839
nvkex <scrbwrm@gmail.com>
2022-02-07 21:46:51 +0530

    Spawn Projectiles  and enemies with movements

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ canvas.height = innerHeight
 
 // Player class with properties
 class Player {
-    constructor(x, y, radius, color){
+    constructor(x, y, radius, color) {
         this.x = x
         this.y = y
         this.radius = radius
@@ -25,7 +25,7 @@ class Player {
 
 // Projectile class
 class Projectile {
-    constructor(x, y, color,radius,velocity){
+    constructor(x, y, radius, color, velocity) {
         this.x = x
         this.y = y
         this.color = color
@@ -39,10 +39,101 @@ class Projectile {
         c.fillStyle = this.color
         c.fill()
     }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.color = color
+        this.velocity = velocity
+        this.radius = radius
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
 }
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 30, 'green')
 player.draw()
+
+const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * 30
+        let x, y
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        }
+        else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+        const color = 'red'
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })
+}
+
+// Event listener for click events on canvas
+// Creates new projectile which will move in the direction of the click
+// from the center/player.
+addEventListener('click', (e) => {
+    const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'yellow',
+        velocity
+    ))
+})
+
+animate()
+spawnEnemies()
